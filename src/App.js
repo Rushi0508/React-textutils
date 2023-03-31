89b429b6e0d74ef4b72fd0d99e848413b7baf828
Rushi_0508 <rushigandhi14@gmail.com>
2023-03-31 12:51:00 +0530

    added dark mode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, {useState} from 'react'
+
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const toggleMode = ()=>{
+    if(mode==="light"){
+      setMode("dark");
+      document.body.style.backgroundColor = "#000000cf"
+    }else{
+      setMode("light")
+      document.body.style.backgroundColor = "white"
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils" />
+      <Navbar title="TextUtils" mode={mode} toggleMode = {toggleMode} />
       <div className="container">
-        <TextForm heading="Enter text to analyze" />
+        <TextForm heading="Enter text to analyze" mode = {mode}/>
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -16,19 +16,19 @@ export default function TextForm(props) {
     return (
         <>
         <div className='container'>
-            <div class="form-group my-3">
+            <div className={`form-group my-3 text-${(props.mode==="light"?"dark":"light")}`}>
                 <h3>{props.heading}</h3>
-                <textarea class="form-control" value={text} placeholder="Enter text here..." onChange={handleChange} rows="6" ></textarea>
+                <textarea className="form-control" value={text} placeholder="Enter text here..." onChange={handleChange} rows="6" ></textarea>
             </div>
             <button className="btn btn-sm btn-primary" onClick={upperCase}> Convert to UpperCase </button>
-            <button className="btn btn-sm btn-dark mx-1" onClick={lowerCase}> Convert to LowerCase </button>
+            <button className="btn btn-sm btn-secondary mx-1" onClick={lowerCase}> Convert to LowerCase </button>
         </div>
-        <div className='container my-3'>
+        <div className={`container my-3 text-${(props.mode==="light"?"dark":"light")}`}>
             <h2>Your Text Summary</h2>
             <p>{text.split(" ").length} words and {text.length} characters</p>
             <p>{Math.ceil(0.008 * text.split(" ").length)} Minute Read</p>
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{(text.length===0)?"Enter text to preview": text}</p>
         </div>
         </>
     )
